2824851980adf0e0bc82f3bc6ceaa35060e6ab57
MGrisafi <MGrisafi@marvelstudios.com>
2023-09-08 16:25:40 -0500

    updating name for Profile

--- pages/Profile.py
+++ pages/Profile.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from datetime import datetime, time
+from datetime import datetime
 
 
 st.title("Miss Minutes Meeting Management")
@@ -8,25 +8,25 @@ with st.form("Miss Minutes Meeting Management"):
     # input for timezone
     timezone = st.text_input("Enter your timezone (e.g., GMT+2):")
 
-    # input for 12 h
+    # input for working hours
     working_hours_start = st.time_input("Working Hours Start Time:", value=datetime(1, 1, 1, 9, 0).time())
     working_hours_end = st.time_input("Working Hours End Time:", value=datetime(1, 1, 1, 17, 0).time())
 
-    # Time input for heads-down time (24-hour format)
+    # input for heads-down time 
     heads_down_start = st.time_input("Heads-Down Start Time:", value=datetime(1, 1, 1, 12, 0).time())
     heads_down_end = st.time_input("Heads-Down End Time:", value=datetime(1, 1, 1, 13, 0).time())
 
-    # Radio button for location (remote or local)
+    # Radio for location (remote or local)
     location = st.radio("Select your location:", ("Remote", "Local"))
 
-    # Time input for preferred meeting time (24-hour format)
+    # input for preferred meeting time
     meeting_start = st.time_input("Preferred Meeting Start Time:", value=datetime(1, 1, 1, 10, 0).time())
     meeting_end = st.time_input("Preferred Meeting End Time:", value=datetime(1, 1, 1, 12, 0).time())
 
-    # Submit button
+    # Submit 
     submit_button = st.form_submit_button(label="Submit")
 
-# Display user input after submission
+# displays input after submission
 if submit_button:
     st.success("User Preferences Submitted!")
     st.write("Timezone:", timezone)
